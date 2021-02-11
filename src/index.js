c27e6239784e5392e2236d24f711b05f0ade80af
Pasha <bayntis@gmail.com>
2021-02-12 00:12:08 +0200

    refact code, delete prevent def from jumMobile

--- src/index.js
+++ src/index.js
@@ -1,250 +1,215 @@
 // import './style/index.scss';
 // import style
 
-const supportsVideo = !!document.createElement('video').canPlayType;
-//if browser can play start
+document.addEventListener('DOMContentLoaded', () => {
+  const supportsVideo = !!document.createElement('video').canPlayType;
+  //if browser can play start
 
-if (supportsVideo) {
-  const body = document.querySelector('body');
-  const volumeBar = document.querySelector('.volumeBar');
-  const videoMuteSoundBtn = document.querySelector('.videoMute');
-  const timeVideo = document.querySelector('.timeVideo');
-  const timeCur = document.querySelector('.timeCur');
-  const videoSpeedBar = document.querySelector('.videoSpeed');
-  const speedBarBtn = document.querySelector('.speedBtn');
-  const progressBar = document.querySelector('#videoProgress-bar');
-  const setingsVideoBtn = document.querySelector('.setingsVideo');
-  const setingsVideoBox = document.querySelector('.setingsVideo-box');
-  const fullscreenBtn = document.querySelector('.btnFullscreen');
-  const videoContainer = document.querySelector('.video-container');
-  const video = document.querySelector('video');
-  const videoControls = document.querySelector('.options');
+  if (supportsVideo) {
+    const body = document.querySelector('body');
+    const volumeBar = document.querySelector('.volumeBar');
+    const videoMuteSoundBtn = document.querySelector('.videoMute');
+    const timeVideo = document.querySelector('.timeVideo');
+    const timeCur = document.querySelector('.timeCur');
+    const videoSpeedBar = document.querySelector('.videoSpeed');
+    const speedBarBtn = document.querySelector('.speedBtn');
+    const progressBar = document.querySelector('#videoProgress-bar');
+    const setingsVideoBtn = document.querySelector('.setingsVideo');
+    const setingsVideoBox = document.querySelector('.setingsVideo-box');
+    const fullscreenBtn = document.querySelector('.btnFullscreen');
+    const videoContainer = document.querySelector('.video-container');
+    const video = document.querySelector('video');
+    const videoControls = document.querySelector('.options');
 
-  const btnPlay = document.querySelectorAll('.btnPlay');
-  const btnPause = document.querySelector('.btnPause');
+    const btnPlay = document.querySelectorAll('.btnPlay');
+    const btnPause = document.querySelector('.btnPause');
 
-  //hide defoult controls
-  video.controls = false;
+    //hide defoult controls
+    video.controls = false;
 
-  //buffering element
-  const buffered = document.createElement('div');
-  buffered.classList.add('buffered');
-  videoControls.appendChild(buffered);
+    //buffering element
+    const buffered = document.createElement('div');
+    buffered.classList.add('buffered');
+    videoControls.appendChild(buffered);
 
-  //for doubleTap event
-  let tapTwice = false;
+    //for doubleTap event
+    let tapTwice = false;
 
-  // Fullscreen mode
-  const fullScreenEnabled = !!(
-    document.fullscreenEnabled ||
-    document.createElement('video').webkitRequestFullScreen
-  );
+    // Fullscreen mode
+    const fullScreenEnabled = !!(
+      document.fullscreenEnabled ||
+      document.createElement('video').webkitRequestFullScreen
+    );
 
-  if (!fullScreenEnabled) {
-    fullscreen.style.display = 'none';
-  }
-
-  const handleFullscreen = function () {
-    if (isFullScreen()) {
-      if (document.exitFullscreen) document.exitFullscreen();
-      setFullscreenData(false);
-    } else {
-      if (videoContainer.requestFullscreen) videoContainer.requestFullscreen();
-      setFullscreenData(true);
+    if (!fullScreenEnabled) {
+      fullscreen.style.display = 'none';
     }
 
-    changeStyleInFullScreen();
-  };
+    const handleFullscreen = function () {
+      if (isFullScreen()) {
+        if (document.exitFullscreen) document.exitFullscreen();
+        setFullscreenData(false);
+      } else {
+        if (videoContainer.requestFullscreen)
+          videoContainer.requestFullscreen();
+        setFullscreenData(true);
+      }
 
-  const isFullScreen = function () {
-    return !!document.fullscreenElement;
-  };
+      changeStyleInFullScreen();
+    };
 
-  const setFullscreenData = function (state) {
-    videoContainer.setAttribute('data-fullscreen', !!state);
-  };
+    const isFullScreen = function () {
+      return !!document.fullscreenElement;
+    };
 
-  function changeStyleInFullScreen() {
-    if (isFullScreen()) {
-      videoControls.classList.remove('hoverVideoControls');
-      fullscreenBtn.classList.remove('hoverVideoControls');
-    } else {
-      videoControls.classList.add('hoverVideoControls');
-      fullscreenBtn.classList.add('hoverVideoControls');
-    }
-  }
+    const setFullscreenData = function (state) {
+      videoContainer.setAttribute('data-fullscreen', !!state);
+    };
 
-  function playOrPause() {
-    if (video.paused || video.ended) {
-      video.play();
-    } else {
-      video.pause();
-    }
-  }
-
-  function changePlayAndPauseButtons() {
-    btnPlay.forEach((btn) => btn.classList.toggle('hidden'));
-    btnPause.classList.toggle('hidden');
-  }
-
-  function videoTime(time) {
-    time = Math.floor(time);
-    let minutes = Math.floor(time / 60);
-    let seconds = Math.floor(time - minutes * 60);
-
-    let minutesVal = minutes;
-    let secondsVal = seconds;
-
-    if (minutes < 10) {
-      minutesVal = '0' + minutes;
+    function changeStyleInFullScreen() {
+      if (isFullScreen()) {
+        videoControls.classList.remove('hoverVideoControls');
+        fullscreenBtn.classList.remove('hoverVideoControls');
+      } else {
+        videoControls.classList.add('hoverVideoControls');
+        fullscreenBtn.classList.add('hoverVideoControls');
+      }
     }
 
-    if (seconds < 10) {
-      secondsVal = '0' + seconds;
+    function playOrPause() {
+      if (video.paused || video.ended) {
+        video.play();
+      } else {
+        video.pause();
+      }
     }
 
-    return minutesVal + ':' + secondsVal;
-  }
-
-  function videoProgress() {
-    let progress =
-      Math.floor(video.currentTime) / (Math.floor(video.duration) / 100);
-    progressBar.value = progress;
-    timeCur.innerHTML = videoTime(video.currentTime);
-  }
-
-  // Set the play position of the video based on the mouse click at x
-  function setPlayPosition(e) {
-    let value = (e.pageX - findPos(progressBar)).toFixed(2);
-    let timeToSet = (
-      (video.duration / progressBar.offsetWidth).toFixed(2) * value
-    ).toFixed(2);
-    video.currentTime = timeToSet;
-  }
-
-  // Find the position in progressBar
-  function findPos(obj) {
-    let curleft = 0;
-    if (obj.offsetParent) {
-      do {
-        curleft += obj.offsetLeft;
-      } while ((obj = obj.offsetParent));
+    function changePlayAndPauseButtons() {
+      btnPlay.forEach((btn) => btn.classList.toggle('hidden'));
+      btnPause.classList.toggle('hidden');
     }
-    return curleft;
-  }
 
-  function changeVolume() {
-    let volume = volumeBar.value / 100;
-    video.volume = volume;
-  }
+    function videoTime(time) {
+      time = Math.floor(time);
+      let minutes = Math.floor(time / 60);
+      let seconds = Math.floor(time - minutes * 60);
 
-  function videoChangeSpeed() {
-    let speed = videoSpeedBar.value / 100;
-    video.playbackRate = speed;
-  }
+      let minutesVal = minutes;
+      let secondsVal = seconds;
 
-  function returnDefoultSpeedValue() {
-    video.playbackRate = 1;
-    videoSpeedBar.value = 100;
-  }
+      if (minutes < 10) {
+        minutesVal = '0' + minutes;
+      }
 
-  function changeVisability(item) {
-    item.classList.toggle('hidden');
-  }
+      if (seconds < 10) {
+        secondsVal = '0' + seconds;
+      }
 
-  //Mute volume
-  function muteVolume() {
-    video.muted = !video.muted;
-    volumeBar.value = 0;
-    videoMuteSoundBtn.classList.add('false');
-
-    if (!video.muted) {
-      videoMuteSoundBtn.classList.remove('false');
-      volumeBar.value = video.volume * 100;
+      return minutesVal + ':' + secondsVal;
     }
-  }
 
-  //Sub Function
-  function startPlayVideo() {
-    playOrPause();
-    changePlayAndPauseButtons();
-
-    if (timeVideo.innerHTML == '00:00') {
-      timeVideo.innerHTML = videoTime(video.duration);
+    function videoProgress() {
+      let progress =
+        Math.floor(video.currentTime) / (Math.floor(video.duration) / 100);
+      progressBar.value = progress;
+      timeCur.innerHTML = videoTime(video.currentTime);
     }
-  }
 
-  //LISTENER
-  btnPlay.forEach((btn) => {
-    btn.addEventListener('click', startPlayVideo);
-  });
-
-  btnPause.addEventListener('click', () => {
-    playOrPause();
-    changePlayAndPauseButtons();
-
-    if (timeVideo.innerHTML == '00:00') {
-      timeVideo.innerHTML = videoTime(video.duration);
+    // Set the play position of the video based on the mouse click at x
+    function setPlayPosition(e) {
+      let value = (e.pageX - findPos(progressBar)).toFixed(2);
+      let timeToSet = (
+        (video.duration / progressBar.offsetWidth).toFixed(2) * value
+      ).toFixed(2);
+      video.currentTime = timeToSet;
     }
-  });
 
-  video.addEventListener('click', () => {
-    playOrPause();
-    changePlayAndPauseButtons();
-
-    if (timeVideo.innerHTML == '00:00') {
-      timeVideo.innerHTML = videoTime(video.duration);
+    // Find the position in progressBar
+    function findPos(obj) {
+      let curleft = 0;
+      if (obj.offsetParent) {
+        do {
+          curleft += obj.offsetLeft;
+        } while ((obj = obj.offsetParent));
+      }
+      return curleft;
     }
-  });
 
-  video.addEventListener('timeupdate', videoProgress);
-
-  progressBar.addEventListener('click', setPlayPosition);
-  progressBar.addEventListener('mouseenter', (e) => {
-    let pos = e.x;
-    let mouseHoverTime = document.createElement('span');
-    let lenghtToVideoContainer =
-      (body.offsetWidth - videoContainer.offsetWidth) / 2;
-    mouseHoverTime.classList.add('mouseHoverTime');
-    let value = (e.pageX - findPos(progressBar)).toFixed(2);
-    let timeToSet = (
-      (video.duration / progressBar.offsetWidth).toFixed(2) * value
-    ).toFixed(2);
-
-    if (!document.querySelector('.mouseHoverTime')) {
-      mouseHoverTime.style.left = pos - lenghtToVideoContainer + 'px';
-      mouseHoverTime.innerText = `${videoTime(timeToSet)}`;
-      videoContainer.append(mouseHoverTime);
-
-      setTimeout(() => {
-        let allMouseHoverTimes = document.querySelectorAll('.mouseHoverTime');
-        allMouseHoverTimes.forEach((item) => item.remove());
-      }, 500);
+    function changeVolume() {
+      let volume = volumeBar.value / 100;
+      video.volume = volume;
     }
-  });
 
-  volumeBar.addEventListener('change', changeVolume);
-  setingsVideoBtn.addEventListener('click', () =>
-    changeVisability(setingsVideoBox)
-  );
-  videoSpeedBar.addEventListener('change', videoChangeSpeed);
-  speedBarBtn.addEventListener('click', returnDefoultSpeedValue);
-  videoMuteSoundBtn.addEventListener('click', muteVolume);
-  fullscreenBtn.addEventListener('click', handleFullscreen);
-  video.addEventListener('dblclick', handleFullscreen);
-
-  //jump 10sec video with arrowL & arrowR
-  document.addEventListener('keydown', function (event) {
-    if (event.key === 'ArrowRight' && video.played) {
-      video.currentTime += 10;
-    } else if (event.key == 'ArrowLeft' && video.played) {
-      video.currentTime -= 10;
+    function videoChangeSpeed() {
+      let speed = videoSpeedBar.value / 100;
+      video.playbackRate = speed;
     }
-  });
 
-  videoContainer.addEventListener(
-    'touchstart',
-    function (e) {
+    function returnDefoultSpeedValue() {
+      video.playbackRate = 1;
+      videoSpeedBar.value = 100;
+    }
+
+    function changeVisability(item) {
+      item.classList.toggle('hidden');
+    }
+
+    //Mute volume
+    function muteVolume() {
+      video.muted = !video.muted;
+      volumeBar.value = 0;
+      videoMuteSoundBtn.classList.add('false');
+
+      if (!video.muted) {
+        videoMuteSoundBtn.classList.remove('false');
+        volumeBar.value = video.volume * 100;
+      }
+    }
+
+    //play | pause
+    function startPlayVideo() {
+      playOrPause();
+      changePlayAndPauseButtons();
+
+      if (timeVideo.innerHTML == '00:00') {
+        timeVideo.innerHTML = videoTime(video.duration);
+      }
+    }
+    //When the User mousenter the Progres bar with the mouse, show the time
+    function TimeProgressHover(e) {
+      let pos = e.x;
+      let mouseHoverTime = document.createElement('span');
+      let lenghtToVideoContainer =
+        (body.offsetWidth - videoContainer.offsetWidth) / 2;
+      mouseHoverTime.classList.add('mouseHoverTime');
+      let value = (e.pageX - findPos(progressBar)).toFixed(2);
+      let timeToSet = (
+        (video.duration / progressBar.offsetWidth).toFixed(2) * value
+      ).toFixed(2);
+
+      if (!document.querySelector('.mouseHoverTime')) {
+        mouseHoverTime.style.left = pos - lenghtToVideoContainer + 'px';
+        mouseHoverTime.innerText = `${videoTime(timeToSet)}`;
+        videoContainer.append(mouseHoverTime);
+
+        setTimeout(() => {
+          let allMouseHoverTimes = document.querySelectorAll('.mouseHoverTime');
+          allMouseHoverTimes.forEach((item) => item.remove());
+        }, 500);
+      }
+    }
+
+    //Jump to 10sec DESCTOP
+    function jumpDesctop(e) {
+      if (e.key === 'ArrowRight' && video.played) {
+        video.currentTime += 10;
+      } else if (e.key == 'ArrowLeft' && video.played) {
+        video.currentTime -= 10;
+      }
+    }
+
+    //Jump to 10sec Mobile
+    function jumpMobile(e) {
       let touch = e.changedTouches;
 
       if (!tapTwice) {
@@ -254,10 +219,9 @@ if (supportsVideo) {
         }, 300);
         return false;
       }
-      e.preventDefault();
 
-      //find dynamic pos video and take he width
-      //if pos touch more then half video container jump to 10s video
+      //find pos videoConatiner and take hes width
+      //if user touch more then half video container jump to 10s video
       let halfVideoContainer = videoContainer.offsetWidth / 2;
       let lenghtToVideoContainer =
         (body.offsetWidth - videoContainer.offsetWidth) / 2;
@@ -267,14 +231,37 @@ if (supportsVideo) {
       } else if (touch[0].pageX - lenghtToVideoContainer < halfVideoContainer) {
         video.currentTime -= 10;
       }
-    },
-    { passive: true }
-  );
-  //Buffering visibility
-  video.addEventListener('progress', function () {
-    let bufferedProces =
-      Math.floor(video.buffered.end(0)) / Math.floor(video.duration);
-    buffered.style.width =
-      Math.floor(bufferedProces * videoContainer.offsetWidth) + 'px';
-  });
-}
+    }
+
+    //LISTENER
+    btnPlay.forEach((btn) => {
+      btn.addEventListener('click', startPlayVideo);
+    });
+    btnPause.addEventListener('click', startPlayVideo);
+    video.addEventListener('click', startPlayVideo);
+    video.addEventListener('timeupdate', videoProgress);
+    progressBar.addEventListener('click', setPlayPosition);
+    progressBar.addEventListener('mouseenter', TimeProgressHover);
+    volumeBar.addEventListener('change', changeVolume);
+    setingsVideoBtn.addEventListener('click', () =>
+      changeVisability(setingsVideoBox)
+    );
+    videoSpeedBar.addEventListener('change', videoChangeSpeed);
+    speedBarBtn.addEventListener('click', returnDefoultSpeedValue);
+    videoMuteSoundBtn.addEventListener('click', muteVolume);
+    fullscreenBtn.addEventListener('click', handleFullscreen);
+    video.addEventListener('dblclick', handleFullscreen);
+    document.addEventListener('keydown', jumpDesctop);
+    videoContainer.addEventListener('touchstart', jumpMobile, {
+      passive: true,
+    });
+
+    //Buffering visibility
+    video.addEventListener('progress', function () {
+      let bufferedProces =
+        Math.floor(video.buffered.end(0)) / Math.floor(video.duration);
+      buffered.style.width =
+        Math.floor(bufferedProces * videoContainer.offsetWidth) + 'px';
+    });
+  }
+});
